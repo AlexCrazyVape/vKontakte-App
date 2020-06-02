8a5fcdec824fa8bb81aebe0c4b2d071efd8c4c1d
Alexander Kosarev(KosSound) <Alexcrazyvape@gmail.com>
2020-06-02 15:51:30 +0300

    test work connection with git hub

---
+++ vKontakte-App/vKontakte-App/vKontakte-App/1.swift
@@ -0,0 +1,13 @@
+//
+//  1.swift
+//  vKontakte-App
+//
+//  Created by Alex.KosSound on 02/06/2020.
+//  Copyright © 2020 Alex.KosSound. All rights reserved.
+//
+
+import UIKit
+
+class _: NSObject {
+
+}
