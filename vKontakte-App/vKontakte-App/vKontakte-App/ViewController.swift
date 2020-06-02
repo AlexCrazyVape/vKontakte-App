755582cc9a9cae11d6c25f0e454a713aa31fa381
AlexCrazyVape <57602647+AlexCrazyVape@users.noreply.github.com>
2020-06-02 15:46:57 +0300

    Add files via upload

---
+++ vKontakte-App/vKontakte-App/vKontakte-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vKontakte-App
+//
+//  Created by Alex.KosSound on 02/06/2020.
+//  Copyright © 2020 Alex.KosSound. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
