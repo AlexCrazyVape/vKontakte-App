481d43bb54171db64c12a34e7bbf8d739382c9e3
Alexander Kosarev(KosSound) <Alexcrazyvape@gmail.com>
2020-06-02 16:04:21 +0300

    Rename ViewController to Login ViewController, delete test file 1.swift.

--- vKontakte-App/vKontakte-App/vKontakte-App/LoginViewController.swift
+++ vKontakte-App/vKontakte-App/vKontakte-App/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  vKontakte-App
 //
 //  Created by Alex.KosSound on 02/06/2020.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
